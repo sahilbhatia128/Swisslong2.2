509bb29cb4347d36f25a6e99d2be7d6cf1cfc0be
Sahil Bhatia <38473274+sahilbhatia128@users.noreply.github.com>
2024-09-04 20:10:45 +0000

    feat: initiated the project

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
-    constructor()ERC20("Swisslong","Sl"){} 
+    constructor()ERC20("bhs","ss"){} 
 
     function mint100tokens() public {
         _mint(msg.sender, 100*10**18);
